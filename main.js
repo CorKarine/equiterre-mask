fbe69c38f375a1cf1e43816f243fd935f99b9eb6
CorKarine <70386393+CorKarine@users.noreply.github.com>
2021-09-22 23:01:26 -0400

    Update main.js
    test pour swiper et son import

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import './node_modules/bootstrap/dist/css/bootstrap.css';
+import Swiper from 'swiper';
+import 'swiper/css';
 import 'gsap';
 import './main.scss';
+const swiper = new Swiper('.swiper-container',{
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 100,
+        disableOnInteraction: false,
+      },
+  })
