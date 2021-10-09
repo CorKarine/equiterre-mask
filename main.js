b2d3a50661150599cbfb1a130f90210888c15c1d
Mab1209 <60610510+Mab1209@users.noreply.github.com>
2021-10-08 20:31:38 -0400

    problemme 404

--- main.js
+++ main.js
@@ -34,4 +34,21 @@ const swiper = new Swiper('.swiper-container',{
     animHamburger.pause(0);
   });
   
+ /* gsap.timeline({
+    delay: 0,
+    repeat: -1,
+    yoyo: false,
+    repeatDelay: 0,
+    ease: "none",
+  })
+
+  .to('.plant',
+    { x: '200%'})
+  .to('.plant',
+    { rotateY: '180'})
+  .to('.plant',
+    { x: '20%'})
+  .to('.plant',
+    { rotateY: '0'})*/
+  
   
